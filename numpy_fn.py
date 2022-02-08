412b81af4552b52ec90c939271aa7b6efcdbf696
sheikhecetufail <sheikhecetufail@gmail.com>
2022-02-08 13:59:46 +0530

    Numpy python file

---
+++ numpy_fn.py
@@ -0,0 +1,7 @@
+import numpy as np
+print(np.array([4,3,5,6]))
+print(np.arange(2,7))
+print(np.arange(2,8,2))
+print(np.linspace(2,7,10))
+print(np.array([[1,2,3],[4,5,6]]))
+print(np.arange(24).reshape(2,3,4))
